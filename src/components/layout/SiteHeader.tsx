b1efab86058453c37dc2f7c8fb420cf8f70dd643
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-08-22 11:56:54 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SlotClone classNa

--- src/components/layout/SiteHeader.tsx
+++ src/components/layout/SiteHeader.tsx
@@ -87,7 +87,7 @@ export function SiteHeader() {
                     {link.label}
                   </Link>
                 ))}
-                <Button asChild size="default" className="font-body text-base w-full mt-4" >
+                <Button asChild size="sm" className="font-body text-sm w-full mt-4" >
                   <Link href="/booking">Book Now</Link>
                 </Button>
               </nav>
