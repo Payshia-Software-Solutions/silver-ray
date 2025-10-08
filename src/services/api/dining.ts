4f239c5a9a6b42a7ec6f2266284c4404d07982c2
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-10-08 06:51:28 +0000

    use this endpoint for getting resturant card https://silverray-server.pa

--- src/services/api/dining.ts
+++ src/services/api/dining.ts
@@ -5,7 +5,8 @@ import { handleApiResponse, cleanImageUrl } from '@/lib/apiClient';
 
 export async function getRestaurants(): Promise<RestaurantFromApi[]> {
   try {
-    const response = await fetch(`${API_BASE_URL}/restaurant`, {
+    const serverRoot = API_BASE_URL.split('/company/')[0];
+    const response = await fetch(`${serverRoot}/restaurant`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
