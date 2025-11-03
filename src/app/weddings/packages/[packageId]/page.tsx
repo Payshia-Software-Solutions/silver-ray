81b7dc292913e73dad2e50b7f8734d1685ae029d
Sandalu Thushan <sandaluthushan20@gmail.com>
2025-11-03 04:03:36 +0000

    this is the jason object data  of {   "id": "26",   "package_name": "Pre

--- src/app/weddings/packages/[packageId]/page.tsx
+++ src/app/weddings/packages/[packageId]/page.tsx
@@ -58,7 +58,7 @@ const getStatusBadgeVariant = (status?: string) => {
             return 'bg-green-100 text-green-700 border-green-300';
         case 'seasonal':
             return 'bg-blue-100 text-blue-700 border-blue-300';
-        case 'coming soon':
+        case 'inactive':
             return 'bg-yellow-100 text-yellow-700 border-yellow-300';
         default:
             return 'bg-gray-100 text-gray-700 border-gray-300';
